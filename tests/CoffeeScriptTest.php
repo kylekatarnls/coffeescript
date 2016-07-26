72e09cff023161a3132a1ef4db36a56f14b578b1
kylekatarnls <kylekatarnls@gmail.com>
2016-07-26 10:38:20 +0200

    More explicit variable name

--- tests/CoffeeScriptTest.php
+++ tests/CoffeeScriptTest.php
@@ -24,10 +24,10 @@ class CoffeeScriptTest extends PHPUnit_Framework_TestCase
     {
         $code = file_get_contents(__DIR__ . '/test.coffee');
         $stylus = new CoffeeScript($code);
-        $js = trim($stylus->getResult());
+        $javascript = trim($stylus->getResult());
         $expected = trim(str_replace("\r", '', file_get_contents(__DIR__ . '/test.js')));
 
-        $this->assertSame($expected, $js, 'CoffeeScript should be rendered anyway.');
+        $this->assertSame($expected, $javascript, 'CoffeeScript should be rendered anyway.');
     }
 
     public function testGetSourceFromPath()
@@ -42,10 +42,10 @@ class CoffeeScriptTest extends PHPUnit_Framework_TestCase
     public function testGetResult()
     {
         $coffee = new CoffeeScript(__DIR__ . '/test.coffee');
-        $js = trim($coffee);
+        $javascript = trim($coffee);
         $expected = trim(str_replace("\r", '', file_get_contents(__DIR__ . '/test.js')));
 
-        $this->assertSame($expected, $js, 'CoffeeScript should be rendered anyway.');
+        $this->assertSame($expected, $javascript, 'CoffeeScript should be rendered anyway.');
     }
 
     public function testEchoResult()
@@ -53,29 +53,29 @@ class CoffeeScriptTest extends PHPUnit_Framework_TestCase
         $coffee = new CoffeeScript(__DIR__ . '/test.coffee');
         ob_start();
         echo $coffee;
-        $js = trim(ob_get_contents());
+        $javascript = trim(ob_get_contents());
         ob_end_clean();
         $expected = trim(str_replace("\r", '', file_get_contents(__DIR__ . '/test.js')));
 
-        $this->assertSame($expected, $js, 'CoffeeScript should be rendered anyway.');
+        $this->assertSame($expected, $javascript, 'CoffeeScript should be rendered anyway.');
     }
 
     public function testNonBareGetResult()
     {
         $coffee = new CoffeeScript(__DIR__ . '/test.coffee', false);
-        $js = preg_replace('/[\s_]/', '', $coffee->getResult());
+        $javascript = preg_replace('/[\s_]/', '', $coffee->getResult());
         $expected = preg_replace('/[\s_]/', '', '(function(){' . file_get_contents(__DIR__ . '/test.js') . '}).call(this);');
 
-        $this->assertSame($expected, $js, 'CoffeeScript should be rendered in a function with node.');
+        $this->assertSame($expected, $javascript, 'CoffeeScript should be rendered in a function with node.');
     }
 
     public function testNonBareGetResultWithoutNode()
     {
         $coffee = new CoffeeScriptWithoutNode(__DIR__ . '/test.coffee', false);
-        $js = preg_replace('/[\s_]/', '', $coffee->getResult());
+        $javascript = preg_replace('/[\s_]/', '', $coffee->getResult());
         $expected = preg_replace('/[\s_]/', '', '(function(){' . file_get_contents(__DIR__ . '/test.js') . '}).call(this);');
 
-        $this->assertSame($expected, $js, 'CoffeeScript should be rendered in a function without node.');
+        $this->assertSame($expected, $javascript, 'CoffeeScript should be rendered in a function without node.');
     }
 
     public function testWrite()
@@ -83,20 +83,20 @@ class CoffeeScriptTest extends PHPUnit_Framework_TestCase
         $file = sys_get_temp_dir() . '/test.js';
         $coffee = new CoffeeScript(__DIR__ . '/test.coffee');
         $coffee->write($file);
-        $js = trim(str_replace("\r", '', file_get_contents($file)));
+        $javascript = trim(str_replace("\r", '', file_get_contents($file)));
         unlink($file);
         $expected = trim(str_replace("\r", '', file_get_contents(__DIR__ . '/test.js')));
 
-        $this->assertSame($expected, $js, 'CoffeeScript should be rendered anyway.');
+        $this->assertSame($expected, $javascript, 'CoffeeScript should be rendered anyway.');
     }
 
     public function testGetResultWithoutNode()
     {
         $expected = trim(preg_replace('/[\r_]/', '', file_get_contents(__DIR__ . '/test.js')));
         $coffee = new CoffeeScriptWithoutNode(__DIR__ . '/test.coffee');
-        $js = trim(preg_replace('/[\r_]/', '', $coffee->getResult()));
+        $javascript = trim(preg_replace('/[\r_]/', '', $coffee->getResult()));
 
-        $this->assertSame($expected, $js, 'CoffeeScript should be rendered anyway.');
+        $this->assertSame($expected, $javascript, 'CoffeeScript should be rendered anyway.');
     }
 
     public function testWriteWithoutNode()
@@ -105,9 +105,9 @@ class CoffeeScriptTest extends PHPUnit_Framework_TestCase
         $coffee = new CoffeeScriptWithoutNode(__DIR__ . '/test.coffee');
         $file = sys_get_temp_dir() . '/test.js';
         $coffee->write($file);
-        $js = trim(preg_replace('/[\r_]/', '', file_get_contents($file)));
+        $javascript = trim(preg_replace('/[\r_]/', '', file_get_contents($file)));
         unlink($file);
 
-        $this->assertSame($expected, $js, 'CoffeeScript should be rendered anyway.');
+        $this->assertSame($expected, $javascript, 'CoffeeScript should be rendered anyway.');
     }
 }
